dae8eac2abb26e76d8605cf5b23b8245e3dd0267
Lucas Trevizan <72326473+LucasTrevizanbr@users.noreply.github.com>
2022-04-01 12:26:19 -0300

    Testando crossOrigin

--- src/main/java/br/com/todo/todo/config/seguranca/ConfiguracaoSeguranca.java
+++ src/main/java/br/com/todo/todo/config/seguranca/ConfiguracaoSeguranca.java
@@ -1,6 +1,5 @@
 package br.com.todo.todo.config.seguranca;
 
-import br.com.todo.todo.model.Usuario;
 import br.com.todo.todo.repository.UsuarioRepository;
 import br.com.todo.todo.service.TokenService;
 import br.com.todo.todo.service.usuario.AutenticacaoService;
@@ -8,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -49,6 +46,7 @@ public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/api/usuarios/cadastrar").permitAll()
                 .antMatchers(HttpMethod.POST,"/api/usuarios/logar").permitAll()
                 .anyRequest().authenticated()
+                .and().httpBasic()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository),
